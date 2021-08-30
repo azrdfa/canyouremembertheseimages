404c9d98a30e4052ee87150d70e145b2ca2d8342
azrdfa <aazhard8@gmail.com>
2021-08-30 11:18:40 +0700

    update: fix bugs double render

--- src/pages/TestPage/TestPage.js
+++ src/pages/TestPage/TestPage.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useInterval } from "../../custom-hooks"
 import {
     CountDown,
@@ -8,49 +8,93 @@ import {
 import "./TestPage.css"
 
 const TestPage = ({ match }) => {
-    const params = { ...match.params }
-    const lastCounter = 9
-    const [counter, setCounter] = useState(0)
-    const [delay, setDelay] = useState(1000)
+    console.log("render TestPage")
     const [isTestStart, setTestStart] = useState(false)
+    const [initBatch, setInitBatch] = useState({
+        loading: true,
+        lastCounter: 0,
+        slideCount: 0
+    })
+    const [intervalBatch, setIntervalBatch] = useState({
+        counter: 0,
+        delay: 1000
+    })
+    const category = match.params.category
+    const difficulty = match.params.difficulty
 
-    useInterval(() => {
-        switch (counter) {
-            case 3:
-                setCounter(counter + 1)
-                setDelay(5000)
+    useEffect(() => {
+        let lastCounter = 0
+        let slideCount = 0
+        switch (difficulty) {
+            case "easy":
+                lastCounter = 9
+                slideCount = 2
                 break
-            case 5:
-                setCounter(counter + 1)
-                setDelay(1000)
+            case "medium":
+                lastCounter = 10
+                slideCount = 3
+                break
+            case "hard":
+                lastCounter = 11
+                slideCount = 4
                 break
             default:
-                setCounter(counter + 1)
                 break
         }
-    }, isTestStart && counter < lastCounter ? delay : null)
+        setInitBatch({
+            loading: false,
+            lastCounter: lastCounter,
+            slideCount: slideCount
+        })
+    }, [difficulty])
+
+    useInterval(() => {
+        switch (intervalBatch.counter) {
+            case 3:
+                setIntervalBatch({
+                    counter: intervalBatch.counter + 1,
+                    delay: 2000
+                })
+                break
+            case 5:
+                setIntervalBatch({
+                    counter: intervalBatch.counter + 1,
+                    delay: 1000
+                })
+                break
+            default:
+                setIntervalBatch({
+                    ...intervalBatch,
+                    counter: intervalBatch.counter + 1
+                })
+                break
+        }
+    }, isTestStart && intervalBatch.counter < initBatch.lastCounter ? intervalBatch.delay : null)
 
     const startTest = () => {
         setTestStart(true)
     }
 
+    if (initBatch.loading) return <h1>Loading ...</h1>
+
     return (
         <div>
             <h1 className="tp-title">TestPage</h1>
             <p>
-                category: {params.category}<br />
-                difficulty: {params.difficulty}
+                category: {category}<br />
+                difficulty: {difficulty}
             </p>
-            <h2>counter: {counter}</h2>
-            <h2>delay: {delay}</h2>
-            {counter < 1 && <h2>iddle</h2>}
-            {counter >= 1 && counter < 4 && <CountDown />}
-            {counter >= 4 && counter < 6 && <ImageSlide />}
-            {counter >= 6 && counter < lastCounter && <CountDown />}
-            {counter >= lastCounter && <QuestionSlide
-                questionCount={2}
-                category={params.category}
-                difficulty={params.difficulty}
+            <h2>counter: {intervalBatch.counter}</h2>
+            <h2>lastCounter: {initBatch.lastCounter}</h2>
+            <h2>delay: {intervalBatch.delay}</h2>
+            {intervalBatch.counter < 1 && <h2>iddle</h2>}
+            {intervalBatch.counter >= 1 && intervalBatch.counter < 4 && <CountDown />}
+            {intervalBatch.counter >= 4 && intervalBatch.counter < 6 && <ImageSlide />}
+            {intervalBatch.counter >= 6 && intervalBatch.counter < initBatch.lastCounter && <CountDown />}
+            {intervalBatch.counter >= initBatch.lastCounter && <QuestionSlide
+                questionCount={initBatch.slideCount}
+                category={category}
+                difficulty={difficulty}
             />}
             {!isTestStart && <button onClick={startTest}>Start Test</button>}
         </div>
