6d27434c77197da8fec5b0bc6a16c90d3e6decef
azrdfa <aazhard8@gmail.com>
2021-09-01 18:40:57 +0700

    add: add title to all page

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import "./App.css"
 const App = () => {
     return (
         <Router>
+            <h1 className="app-text-center">Can You Remember These Images?</h1>
             <Switch>
                 <Route path="/" exact component={HomePage} />
                 <Route path="/test/:category/:difficulty" component={TestPage} />
