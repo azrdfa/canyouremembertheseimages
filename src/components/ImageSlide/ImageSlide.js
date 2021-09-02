718dfe95b90d59674b7e663e1ceb2220c7fbbf93
azrdfa <aazhard8@gmail.com>
2021-09-02 21:42:06 +0700

    update: restyle ImageSlide

--- src/components/ImageSlide/ImageSlide.js
+++ src/components/ImageSlide/ImageSlide.js
@@ -6,20 +6,20 @@ const ImageSlide = ({ category, difficulty }) => {
     const imagesPath = process.env.PUBLIC_URL + `/assets/images/${category}/right`
     const counter = useRef(0)
     const increaseCounter = () => {
-        const result = <div>
-            <img
-                className="is-img"
-                src={`${imagesPath}/right-${counter.current}.jpeg`}
-                alt={`${imagesPath}/right-${counter.current}.jpeg`}
-            />
-        </div>
+        const result = <img
+            className="is-img"
+            src={`${imagesPath}/right-${counter.current}.jpeg`}
+            alt={`${imagesPath}/right-${counter.current}.jpeg`}
+        />
         counter.current += 1
         return result
     }
     return (
-        <div>
-            {increaseCounter()}
-            <h2 className="is-text-center">{cardinalToOrdinal(counter.current)} Image</h2>
+        <div className="is-vp">
+            <div className="is-flex-container">
+                {increaseCounter()}
+                <h2 className="is-text-center">{cardinalToOrdinal(counter.current)} Image</h2>
+            </div>
         </div>
     )
 }
