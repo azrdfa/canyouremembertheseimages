e7fe21b2d501d7071f4162e8155f262f3a4e7fb0
azrdfa <aazhard8@gmail.com>
2021-09-05 09:23:56 +0700

    update: restyle ResultPage

--- src/pages/ResultPage/ResultPage.js
+++ src/pages/ResultPage/ResultPage.js
@@ -3,13 +3,35 @@ import "./ResultPage.css"
 
 const ResultPage = ({ match }) => {
     const params = { ...match.params }
+    const feedback = () => {
+        const percentage = params.rightAnswer / params.totalQuestion
+        if (percentage === 1) {
+            return <h2 className="rp-h2">
+                Perfect Score!<br />
+                You beat the challange
+            </h2>
+        } else if (percentage >= 0.75) {
+            return <h2 className="rp-h2">
+                A little bit more!<br />
+                You almost beat the challange
+            </h2>
+        } else {
+            return <h2 className="rp-h2">
+                Better luck next time!<br />
+                You can do better than this
+            </h2>
+        }
+    }
+
     return (
-        <div>
-            <h1 className="rp-title">ResultPage</h1>
-            <h2>Score: {params.rightAnswer}/{params.totalQuestion}</h2>
-            <LinkContainer to="/">
-                <button>to HomePage</button>
-            </LinkContainer>
+        <div className="rp-vp">
+            <div className="rp-flex-container">
+                <h2 className="rp-h0">{params.rightAnswer}/{params.totalQuestion}</h2>
+                {feedback()}
+                <LinkContainer to="/">
+                    <button className="rp-btn">Go to Home Page</button>
+                </LinkContainer>
+            </div>
         </div>
     )
 }
