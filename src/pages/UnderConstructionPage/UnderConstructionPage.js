2e9d47cb1c255490d89f5cb0642b86db3de09f52
azrdfa <aazhard8@gmail.com>
2021-09-15 18:13:21 +0700

    add UnderConstructionPage

---
+++ src/pages/UnderConstructionPage/UnderConstructionPage.js
@@ -0,0 +1,14 @@
+import "./UnderConstructionPage.css"
+import { HomePageButton } from "../../components"
+
+const UnderConstructionPage = () => {
+    return (
+        <div className="ucp-container">
+            <h1 className="ucp-h0">COMING SOON</h1>
+            <h1 className="ucp-h1">This page is under construction</h1>
+            <HomePageButton />
+        </div>
+    )
+}
+
+export default UnderConstructionPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import HomePage from "./HomePage"
 import TestPage from "./TestPage"
+import NotFoundPage from "./NotFoundPage/NotFoundPage"
+import UnderConstructionPage from "./UnderConstructionPage/UnderConstructionPage"
 
 export {
     HomePage,
-    TestPage
+    TestPage,
+    NotFoundPage,
+    UnderConstructionPage
 }
