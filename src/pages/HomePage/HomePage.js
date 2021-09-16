a244ab7ad9467152b4a8c52feb1acf94e9e1393c
azrdfa <aazhard8@gmail.com>
2021-09-16 08:18:46 +0700

    refactor buggy style (Buggy style in HomePage)

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { LinkContainer } from "react-router-bootstrap"
-import { restyleCategory } from "../../utils"
-import { BiLeftArrowAlt } from "react-icons/bi"
+import { GoChevronLeft } from "react-icons/go"
+import longText from "../../constants/longText"
 import "./HomePage.css"
 
 const imagesPath = process.env.PUBLIC_URL + `/assets/images`
@@ -23,113 +23,94 @@ const HomePage = () => {
     })
   }
   return (
-    <div>
-      <p className="hp-p">
-        This web is a challange for your brain. Here's how it works.
-        There's 2 phase. First, the remembering phase.
-        you'll be shown some images and you have to remember as many image as possible.
-        Second, the question phase. There will be 2 images that appear for each question.
-        One is an image that you have seen and the other that you have not seen.
-        Choose an image you've seen.
-      </p>
-      {initBatch.showCategory &&
-        <div>
-          <h2 className="hp-center-text">Select Category</h2>
+    <div className="hp-container">
+      <h1 className="hp-h1">Can You Remember These Images?</h1>
+      <p className="hp-p">{longText.main_description}</p>
+      {
+        initBatch.showCategory &&
+        <>
+          <h1 className="hp-h1-sc">Select Category</h1>
           <div className="hp-flex-container">
-            <div className="hp-flex-item">
-              <div
-                className="hp-flex-item-card"
-                onClick={() => setCategory("brand_logo")}
-              >
-                <img
-                  className="hp-img"
-                  src={`${imagesPath}/category/brand_logo.jpg`}
-                  alt={`${imagesPath}/category/brand_logo.jpg`}
-                />
-              </div>
-              <h3 className="hp-center-text">Brand Logo</h3>
+            <div
+              className="hp-flex-item"
+              onClick={() => setCategory("brand_logo")}
+            >
+              <img
+                className="hp-img"
+                src={`${imagesPath}/category/brand_logo.jpg`}
+                alt={`${imagesPath}/category/brand_logo.jpg`}
+              />
+              <h3 className="hp-h3">Brand Logo</h3>
             </div>
-            <div className="hp-flex-item">
-              <div
-                className="hp-flex-item-card"
-                onClick={() => setCategory("anime")}
-              >
-                <img
-                  className="hp-img"
-                  src={`${imagesPath}/category/anime.jpg`}
-                  alt={`${imagesPath}/category/anime.jpg`}
-                />
-              </div>
-              <h3 className="hp-center-text">Anime</h3>
+            <div
+              className="hp-flex-item"
+              onClick={() => setCategory("anime")}
+            >
+              <img
+                className="hp-img"
+                src={`${imagesPath}/category/anime.jpg`}
+                alt={`${imagesPath}/category/anime.jpg`}
+              />
+              <h3 className="hp-h3">Anime</h3>
             </div>
-            <div className="hp-flex-item">
-              <div
-                className="hp-flex-item-card"
-                onClick={() => setCategory("video_game")}
-              >
-                <img
-                  className="hp-img"
-                  src={`${imagesPath}/category/video_game.jpg`}
-                  alt={`${imagesPath}/category/video_game.jpg`}
-                />
-              </div>
-              <h3 className="hp-center-text">Video Game</h3>
+            <div
+              className="hp-flex-item"
+              onClick={() => setCategory("video_game")}
+            >
+              <img
+                className="hp-img"
+                src={`${imagesPath}/category/video_game.jpg`}
+                alt={`${imagesPath}/category/video_game.jpg`}
+              />
+              <h3 className="hp-h3">Video Game</h3>
             </div>
           </div>
-        </div>
+        </>
       }
       {
         initBatch.showCategory === false &&
-        <div>
-          <h2 className="hp-center-text">Select Difficulty</h2>
+        <>
+          <h1 className="hp-h1-sc">Select Difficulty</h1>
           <div className="hp-flex-container">
-            <div className="hp-flex-item">
-              <LinkContainer to={`test/${initBatch.category}/easy`}>
-                <div className="hp-flex-item-card">
-                  <img
-                    className="hp-img"
-                    src={`${imagesPath}/difficulty/easy.jpg`}
-                    alt={`${imagesPath}/difficulty/easy.jpg`}
-                  />
-                </div>
-              </LinkContainer>
-              <h3 className="hp-center-text">{restyleCategory(initBatch.category)} Easy</h3>
-            </div>
-            <div className="hp-flex-item">
-              <LinkContainer to={`test/${initBatch.category}/medium`}>
-                <div className="hp-flex-item-card">
-                  <img
-                    className="hp-img"
-                    src={`${imagesPath}/difficulty/medium.jpg`}
-                    alt={`${imagesPath}/difficulty/medium.jpg`}
-                  />
-                </div>
-              </LinkContainer>
-              <h3 className="hp-center-text">{restyleCategory(initBatch.category)} Medium</h3>
-            </div>
-            <div className="hp-flex-item">
-              <LinkContainer to={`test/${initBatch.category}/hard`}>
-                <div className="hp-flex-item-card">
-                  <img
-                    className="hp-img"
-                    src={`${imagesPath}/difficulty/hard.jpg`}
-                    alt={`${imagesPath}/difficulty/hard.jpg`}
-                  />
-                </div>
-              </LinkContainer>
-              <h3 className="hp-center-text">{restyleCategory(initBatch.category)} Hard</h3>
-            </div>
+            <LinkContainer to={`test/${initBatch.category}/easy`}>
+              <div className="hp-flex-item">
+                <img
+                  className="hp-img"
+                  src={`${imagesPath}/difficulty/easy.jpg`}
+                  alt={`${imagesPath}/difficulty/easy.jpg`}
+                />
+                <h3 className="hp-h3">Easy</h3>
+              </div>
+            </LinkContainer>
+            <LinkContainer to={`test/${initBatch.category}/medium`}>
+              <div className="hp-flex-item">
+                <img
+                  className="hp-img"
+                  src={`${imagesPath}/difficulty/medium.jpg`}
+                  alt={`${imagesPath}/difficulty/medium.jpg`}
+                />
+                <h3 className="hp-h3">Medium</h3>
+              </div>
+            </LinkContainer>
+            <LinkContainer to={`test/${initBatch.category}/hard`}>
+              <div className="hp-flex-item">
+                <img
+                  className="hp-img"
+                  src={`${imagesPath}/difficulty/hard.jpg`}
+                  alt={`${imagesPath}/difficulty/hard.jpg`}
+                />
+                <h3 className="hp-h3">Hard</h3>
+              </div>
+            </LinkContainer>
           </div>
-          <div className="hp-center-div">
-            <div
-              className="hp-text-icon"
-              onClick={resetCategory}
-            >
-              <BiLeftArrowAlt className="hp-icon" />
-              <h3>Select Category</h3>
-            </div>
-          </div>
-        </div>
+          <h2
+            className="hp-h2"
+            onClick={resetCategory}
+          >
+            <GoChevronLeft className="hp-icon"/>
+            Select Category
+          </h2>
+        </>
       }
     </div>
   )
